063e34342be6a6e116786583b05e26c96a29d68b
Oleksandr Khomiak <khomiak@gmail.com>
2023-05-23 21:22:49 +0300

    fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template111
+    <div>
+      <div>
+        <span>Home</span>
+        <span>Movies</span>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types'
+
+const Home = () => {
+  const [trendingFilms, setTrendingFilms] = useState([]);
+
+  useEffect(() => {
+    const API_KEY = 'f051ac50d3bfe0c3fd75f02c1ff7b688';
+    const BASE_URL = 'https://api.themoviedb.org/';
+    const URL = `${BASE_URL}3/trending/all/day?api_key=${API_KEY}`;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setTrendingFilms(data.results);
+      });
+  }, []);
+
+  // Верстка
+  return (
+    <div>
+      <h1>Trending today</h1>
+      <ol>
+        {trendingFilms.map(film => {
+          // якщо назва фільму в параметрі film.title
+          if (film.title) {
+            return (
+              <li key={film.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+                  width={`100`}
+                  alt="preview"
+                />
+                <p>{film.title}</p>
+              </li>
+            );
+          }
+          // якщо назва фільму в параметрі film.name
+          if (film.name) {
+            return (
+              <li key={film.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+                  width={`100`}
+                  alt="preview"
+                />
+                <p>{film.name}</p>
+              </li>
+            );
+          }
+        })}
+      </ol>
+    </div>
+  );
+};
+
+// Home.propTypes = {}
+
+export default Home;
