b2f416603df51a0ce92439b6c011451d000ff7f3
sasha-khomiak <113462446+sasha-khomiak@users.noreply.github.com>
2023-05-30 07:52:54 +0000

    Deploying to gh-pages from @ sasha-khomiak/goit-react-hw-05-movies@0c85c1ac02ead527201d48e251f22778cae9328d 🚀

---
+++ static/js/217.0a30af03.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[217],{2217:function(e,t,i){i.r(t);var n=i(184);t.default=function(){return(0,n.jsx)("div",{children:"PageNotFound"})}}}]);
+//# sourceMappingURL=217.0a30af03.chunk.js.map
